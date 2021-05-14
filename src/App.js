d758b21a8e66656ddaa2a13270acb408260574d5
danielwilfredo <xvwilito11@gmail.com>
2021-05-14 16:32:23 -0600

    agregando una imagen xD :v

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import "tailwindcss/tailwind.css";
 function App() {
   return (
     <>
-    <h1 className="italic text-blue-800" >Hola chichico</h1>
+    <img className="object-contain h-48 w-full mt-6" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png" alt="logo"/>
+    <h1 className="text-4xl text-center mt-6 text-blue-800 " >¡Bienvenido a tu buscador Pokemon!</h1>
+    <div class="flex justify-center mt-6">
+        <div>
+          <label for="first_name">First Name</label>
+          <input type="text" name="first_name" id="first_name"/>
+        </div>
+</div>
+
+    
     </>
   );
 }
