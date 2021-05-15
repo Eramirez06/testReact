abf71cbd97e42c9dc8cda6204dfc9b604d4215f5
danielwilfredo <xvwilito11@gmail.com>
2021-05-14 20:24:10 -0600

    no me sirvio el form :c

--- src/App.js
+++ src/App.js
@@ -4,16 +4,37 @@ import "tailwindcss/tailwind.css";
 function App() {
   return (
     <>
-    <img className="object-contain h-48 w-full mt-6" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png" alt="logo"/>
-    <h1 className="text-4xl text-center mt-6 text-blue-800 " >¡Bienvenido a tu buscador Pokemon!</h1>
-    <div class="flex justify-center mt-6">
-        <div>
-          <label for="first_name">First Name</label>
-          <input type="text" name="first_name" id="first_name"/>
-        </div>
-</div>
-
-    
+      <img
+        className="object-contain h-48 w-full mt-6"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png"
+        alt="logo"
+      />
+      <h1 className="text-4xl text-center mt-6 text-white">
+        ¡Bienvenido a tu buscador Pokémon!
+      </h1>
+      <div className="flex justify-center mt-6">
+        <form className="mb-6" action="/" method="post">
+          <div class="flex flex-col mb-4">
+            <label
+              className="mb-2 uppercase font-bold text-lg text-grey-darkest"
+              for="pokemon"
+            >
+              Pokemon a buscar:
+            </label>
+            <input
+              className="border py-2 px-3 text-grey-darkest"
+              type="text"
+              name="pokemon"
+              id="pokemon"
+            />
+          </div>
+          <input className="transform motion-safe:hover:scale-110"
+          type="submit"
+          value=" Buscar Pokémon" />
+ 
+        </form>
+      </div>
+      
     </>
   );
 }
